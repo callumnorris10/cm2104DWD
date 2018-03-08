ac1193a429264e00337765ebab2f8787a26cdb81
1602395 <callumnorris10@googlemail.com>
2018-03-08 09:24:38 +0000

    additem initial commit

---
+++ lab11/addItem.js
@@ -0,0 +1,14 @@
+$(function(){
+  //document ready message
+  alert("document ready");
+
+    $('#searchform').submit(function(){
+      addItemToList("example item");
+      return false;
+
+    });
+});
+
+function addItemToList(item){
+  $('#results').append("<li>" + item + "</li>");
+}
