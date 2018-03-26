48805adcbab6c863b4ffcc8c854670a8da63e464
1602395 <callumnorris10@googlemail.com>
2018-03-26 16:26:21 +0100

    /

--- lab17/views/server.js
+++ lab17/views/server.js
@@ -8,11 +8,24 @@ app.set('view engine', 'ejs');
 
 //Now use res.render to load up an EJS-view file
 
-//index
-app.get('/', function(req,res){
-  res.render('pages/index');
-});
+// //index
+// app.get('/', function(req,res){
+//   res.render('pages/index');
+// });
 
+//index(new way)
+app.get('/',function(req,res){
+  var drinks = [
+    {name: 'Bloody Mary',drunkness: 3},
+    {name: 'Martini',drunkness: 5},
+    {name: 'Whiskey',drunkness: 10},
+  ];
+  var tagline="asdlk;asfslkalfalkflsalkfssafs";
+  res.render('pages/index',{
+    drinks: drinks,
+    tagline: tagline
+  });
+});
 //about
 app.get('/about', function(req,res){
   res.render('pages/about;');
